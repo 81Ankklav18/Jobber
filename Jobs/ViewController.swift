c51cbb869c3868493105dc226f1e0cfd07aa733d
Anklav <81anklav18@gmail.com>
2019-03-25 20:03:32 +0300

    Initial Commit

---
+++ Jobs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jobs
+//
+//  Created by Егор Рябчук on 25/03/2019.
+//  Copyright © 2019 Егор Рябчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
